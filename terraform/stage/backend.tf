0946b0f088883d0fe12a8b11e7d5b0ce2795cc30
Batt1eMercy <maxim13082000@gmail.com>
2022-07-06 14:45:28 +0300

    add backend

---
+++ terraform/stage/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    endpoint                    = "storage.yandexcloud.net"
+    bucket                      = "otus-terra"
+    region                      = "ru-central1-a"
+    key                         = "stage/terraform.tfstate"
+    access_key                  = "aaaaaaaaaa"
+    secret_key                  = "aaaaaaaaaa"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
---
+++ terraform/storage-bucket.tf
@@ -0,0 +1,21 @@
+#terraform {
+#  required_providers {
+#    yandex = {
+#      source = "yandex-cloud/yandex"
+#   }
+#  }
+#}
+
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+
+resource "yandex_storage_bucket" "otus-terra" {
+  bucket        = var.bucket_name
+  access_key    = var.access_key
+  secret_key    = var.secret_key
+  force_destroy = "true"
+}
